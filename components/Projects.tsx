559bb1b448e9e69c93831355a6b45c57d6b95518
YuriFerdinand <muhammadyuri965@gmail.com>
2026-05-24 23:05:22 +0700

    final card project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,28 +1,54 @@
 import { GoArrowUpRight } from "react-icons/go"; 
+import Image from "next/image";
 
 export default function Projects() {
+    const items = [
+        {
+            image:"/image1.jpg",
+            pName:"Website Program Studi STTI Tanjungpinang",
+            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
+            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
+            link:"#",
+        },
+        {
+            image:"/image1.jpg",
+            pName:"Website Program Studi STTI Tanjungpinang",
+            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
+            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
+            link:"#",
+        },
+        {
+            image:"/image1.jpg",
+            pName:"Website Program Studi STTI Tanjungpinang",
+            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
+            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
+            link:"#",
+        },
+    ]
     return (
-        <div className="bg-[#202020] w-auto h-100 rounded-md group">
-            <div className="bg-[#2a2a2a] rounded-lg h-50 w-auto m-4 group-hover:h-44 transition-[height] duration-300 ease-in-out relative overflow-hidden border border-white/10 flex justify-center items-center">
-                Image
-                <div className="absolute top-0 right-0 w-8 h-8 border-t-2 border-r-2 border-[#c9a96e] rounded-tr-lg" />
-                <div className="absolute bottom-0 left-0 w-8 h-8 border-b-2 border-l-2 border-[#c9a96e] rounded-bl-lg" />
-            </div>
-            <div className="mx-4 ">
-                <h3 className="text-xl font-bold truncate my-1">Project Name</h3>
-                <h3 className="text-md text-[#c9a96e] font-light my-1">Company Name</h3>
-                <p className="text-sm text-[#707070] leading-relaxed line-clamp-3 my-1">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit, pariatur!
-                </p>
-                <div className="my-2 opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-300">
-                    <a href="#">
-                        <span className="text-md font-semibold hover:text-[#c9a96e]">
-                          View More
-                          <GoArrowUpRight className="inline-block ml-1" />
-                        </span>
-                    </a>
+        <div className="flex flex col gap-4">
+            {items.map((item, i) => (
+                <div className="bg-[#202020] w-full h-100 rounded-md group" key={i}>
+                    <div className="bg-[#2a2a2a] rounded-lg h-50 w-auto m-4 group-hover:h-44 transition-[height] duration-300 ease-in-out relative overflow-hidden border border-white/10 flex justify-center items-center">
+                        <Image src={item.image} alt="" fill className="object-cover"/>
+                        <div className="absolute top-0 right-0 w-8 h-8 border-t-2 border-r-2 border-[#c9a96e] rounded-tr-lg" />
+                        <div className="absolute bottom-0 left-0 w-8 h-8 border-b-2 border-l-2 border-[#c9a96e] rounded-bl-lg" />
+                    </div>
+                    <div className="mx-4 flex flex-col gap-2">
+                        <h3 className="text-xl font-bold truncate">{item.pName}</h3>
+                        <h3 className="text-md text-[#c9a96e] font-light">{item.cName}</h3>
+                        <p className="text-sm text-[#707070] leading-relaxed line-clamp-3">{item.desc}</p>
+                        <div className="opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-300">
+                            <a href={`${item.link}`}>
+                                <span className="text-md font-semibold hover:text-[#c9a96e]">
+                                    View More
+                                    <GoArrowUpRight className="inline-block ml-1" />
+                                </span>
+                            </a>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            ))}
         </div>
     );
 }

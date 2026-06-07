4ee03b62f9b4482ad0e746f780764f8cda08bdf3
YuriFerdinand <muhammadyuri965@gmail.com>
2026-06-07 14:35:52 +0700

    card project baru

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,50 +1,90 @@
-import { GoArrowUpRight } from "react-icons/go"; 
+import { GoArrowUpRight } from "react-icons/go";
+import { FaReact, FaLaravel } from "react-icons/fa";
+import { RiNextjsFill } from "react-icons/ri";
+import { SiMysql } from "react-icons/si";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaPhp } from "react-icons/fa";
 import Image from "next/image";
+import Link from "next/link";
+
+export const items = [
+        {
+            image:"/image1.jpg",
+            type:"Intern Project",
+            title:"STTI Tanjungpinang's Study Program Website",
+            link:"/detail/web-prodi",
+            year:"2025",
+            desc:[
+                "A website that built from demand on each study program to show more information and anouncement from lecturer for students about ongoing activites on STTI Tanjungpinang.", 
+                "This website is my first work that used by profesional agency and i was studying to be a profesional developer there. I have done many trouble for building this website, like a dissent with partner, a ghost error, a problem to use other library, and more.",
+                "Even though this project have a lot of problem, i have learned so many valueable lesson about how to use Next.js, database ecosystem, deploying, etc."
+            ],
+            icon:[
+                { name: "Next.js", icon:<RiNextjsFill size={24} color="#000000" /> },
+                { name: "Laravel", icon:<FaLaravel size={24} color="#F05340" /> },
+                { name: "MySQL", icon:<SiMysql size={24} color="#4479A1" />},
+            ]
+        },
+        {
+            image:"/image1.jpg",
+            type:"School Project",
+            title:"GusExpress",
+            desc:[
+                "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab exercitationem ex ratione veritatis culpa sed sapiente reprehenderit labore soluta, iste, reiciendis enim, beatae quasi aspernatur.",
+            ],
+            link:"#",
+            icon:[
+                { name: "PHP", icon: <FaPhp size={24} color="#4F5B93" /> },
+                { name: "MySQL", icon: <SiMysql size={24} color="#4479A1" />},
+                { name: "Javascript", icon: <IoLogoJavascript size={24} color="#F0DB4F" />},
+            ]
+        },
+    ];
 
 export default function Projects() {
-    const items = [
-        {
-            image:"/image1.jpg",
-            pName:"Website Program Studi STTI Tanjungpinang",
-            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
-            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
-            link:"#",
-        },
-        {
-            image:"/image1.jpg",
-            pName:"Website Program Studi STTI Tanjungpinang",
-            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
-            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
-            link:"#",
-        },
-        {
-            image:"/image1.jpg",
-            pName:"Website Program Studi STTI Tanjungpinang",
-            cName:"Sekolah Tinggi Teknologi Indonesia Tanjungpinang",
-            desc:"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum reiciendis tempora quos sint adipisci dolorem ducimus deserunt quam ratione atque! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odit consequatur corrupti totam voluptatibus itaque numquam molestiae incidunt ipsam natus inventore!",
-            link:"#",
-        },
-    ]
+
     return (
-        <div className="flex flex col gap-4">
+        <div className="grid grid-cols-3 gap-4" id="projects-grid">
             {items.map((item, i) => (
-                <div className="bg-[#202020] w-full h-100 rounded-md group" key={i}>
-                    <div className="bg-[#2a2a2a] rounded-lg h-50 w-auto m-4 group-hover:h-44 transition-[height] duration-300 ease-in-out relative overflow-hidden border border-white/10 flex justify-center items-center">
-                        <Image src={item.image} alt="" fill className="object-cover"/>
-                        <div className="absolute top-0 right-0 w-8 h-8 border-t-2 border-r-2 border-[#c9a96e] rounded-tr-lg" />
-                        <div className="absolute bottom-0 left-0 w-8 h-8 border-b-2 border-l-2 border-[#c9a96e] rounded-bl-lg" />
-                    </div>
-                    <div className="mx-4 flex flex-col gap-2">
-                        <h3 className="text-xl font-bold truncate">{item.pName}</h3>
-                        <h3 className="text-md text-[#c9a96e] font-light">{item.cName}</h3>
-                        <p className="text-sm text-[#707070] leading-relaxed line-clamp-3">{item.desc}</p>
-                        <div className="opacity-0 group-hover:opacity-100 ease-in-out transition-all duration-300">
-                            <a href={`${item.link}`}>
-                                <span className="text-md font-semibold hover:text-[#c9a96e]">
-                                    View More
-                                    <GoArrowUpRight className="inline-block ml-1" />
-                                </span>
-                            </a>
+                <div key={i}  
+                    data-aos="fade-up" 
+                    data-aos-delay={i * 300}
+                    data-aos-duration="900"
+                    data-aos-easing="ease-out-cubic"
+                    data-aos-anchor="#projects-grid">
+                    <div 
+                        className="bg-[#202020] w-full h-100 rounded-md group 
+                                border border-[#2a2a2a] 
+                                hover:border-[#c9a96e]
+                                hover:-translate-y-2 transition-[transform, border-color] duration-300 ease-out
+                                will-change-transform" 
+                        style={{ overflowAnchor: 'none' }}
+                    >
+                        <div className="bg-gray-900 h-[50%] rounded-t-md p-4">
+                            <div className="p-2 bg-[#c9a96e]/20 rounded-lg size-fit">
+                                <p className="text-sm text-[#c9a96e] font-semibold">{item.type}</p>
+                            </div>
+                        </div>
+                        <div className="h-[50%] p-4 flex flex-col justify-between">
+                            <div className="flex flex-col gap-2">
+                                <h3 className="text-lg truncate">{item.title}</h3>
+                                <p className="line-clamp-3 text-gray-400 text-md">{item.desc[0]}</p>
+                            </div>
+                            <hr className="border-t-[#2a2a2a] border-t-2" />
+                            <div className="flex flex-row justify-between">
+                                <div className="flex flex-row gap-2">
+                                    {item.icon.map((icon, index) => (
+                                        <div key={index} className="border flex flex-row gap-1 items-center text-xs p-1 rounded-lg bg-[#1a1a1a] border-[#333333]">
+                                            <span>{icon.icon}</span>
+                                            <p className="text-[#aaaaaa]">{icon.name}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                                <Link href={item.link} className="flex flex-row items-center justify-end text-[#c9a96e] font-semibold">
+                                    <p>View More</p>
+                                    <GoArrowUpRight size={24} />
+                                </Link>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -52,6 +92,3 @@ export default function Projects() {
         </div>
     );
 }
-
-
-
